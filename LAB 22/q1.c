a682ebc7e319330a1dddca1fe31cd58852724c95
dhairya 138 <295071029+dhairyad138@users.noreply.github.com>
2026-09-20 21:51:41 +0530

    Add files via upload

---
+++ LAB 22/q1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int binary_search(int arr, int left, int right)
+{
+if(left > right)
+{
+    return -1
+}
+int middle = (left+right)/2
+if(key == arr(middle))
+{
+    return middle+1
+}
+else if()
+}
